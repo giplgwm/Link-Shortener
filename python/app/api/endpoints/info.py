224b459d78fb2c74c5188326d87bea872947ae24
gip <98342753+giplgwm@users.noreply.github.com>
2026-05-15 01:06:39 +0000

    Revert "Fix /info and /redirect schema imports"
    This reverts commit 2b71b6df43f9644a1066b96e51c043a741ade373.

--- python/app/api/endpoints/info.py
+++ python/app/api/endpoints/info.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
-from app.schemas import SlugFromUser
 
 router = APIRouter()
 
 @router.get("/info/{slug}")
-async def info(slug: SlugFromUser):
+async def info(slug: Slug):
     # Return info about a shortened link
     pass
--- python/app/api/endpoints/redirect.py
+++ python/app/api/endpoints/redirect.py
@@ -1,15 +1,12 @@
-from http import HTTPStatus
 from fastapi import APIRouter
-from fastapi.responses import RedirectResponse
-from app.schemas import SlugFromUser
 
 router = APIRouter()
 
 @router.get("/{slug}")
-async def redirect(slug: SlugFromUser):
+async def redirect(slug: Slug):
     # Redirect to original url
-    original_url = None
+    pass
     # if slug in db:
-    return RedirectResponse(original_url, HTTPStatus.TEMPORARY_REDIRECT)
+        return RedirectResponse(original_url, HTTPStatus.TEMPORARY_REDIRECT)
     # if slug not in db:
-    return RedirectResponse("/404", HTTPStatus.TEMPORARY_REDIRECT)
+        return RedirectResponse("/404", HTTPStatus.TEMPORARY_REDIRECT)
