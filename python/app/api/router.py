4156a95ecbed55b2b6de026c2713f327d2cff925
gip <98342753+giplgwm@users.noreply.github.com>
2026-05-15 00:15:03 +0000

    fix typos

--- python/app/api/router.py
+++ python/app/api/router.py
@@ -7,5 +7,5 @@ APIrouter.include_router(info.router)
 APIrouter.include_router(notfound.router)
 APIrouter.include_router(root.router)
 APIrouter.include_router(shorten.router)
-# Redirect comes last to make sure /{slug} does not take priority for matching over names routes
+# Redirect comes last to make sure /{slug} does not take priority for matching over named routes
 APIrouter.include_router(redirect.router)
