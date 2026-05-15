22d277b4aa017485b885b3178a993f622f89db16
gip <98342753+giplgwm@users.noreply.github.com>
2026-05-15 03:25:00 +0000

    Implement /info

--- python/app/api/endpoints/info.py
+++ python/app/api/endpoints/info.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from app.db import queries
+from app.schemas import LinkToUser
+from http import HTTPStatus
 
 router = APIRouter()
 
-@router.get("/info/{slug}")
+@router.get("/info/{slug}", response_model=LinkToUser)
 async def info(slug: str):
-    # Return info about a shortened link
-    pass
+    result = queries.get_link_info(slug=slug)
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Link not found")
--- python/app/api/router.py
+++ python/app/api/router.py
@@ -3,8 +3,8 @@ from app.api.endpoints import info, notfound, redirect, root, shorten
 
 APIrouter = APIRouter()
 
-# APIrouter.include_router(info.router)
-# APIrouter.include_router(notfound.router)
+APIrouter.include_router(info.router)
+APIrouter.include_router(notfound.router)
 APIrouter.include_router(root.router)
 APIrouter.include_router(shorten.router)
 # # Redirect comes last to make sure /{slug} does not take priority for matching over named routes
