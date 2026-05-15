3ce2a6dd82ad2c6b9767585f71c465dc1681da58
gip <98342753+giplgwm@users.noreply.github.com>
2026-05-15 02:53:59 +0000

    Add retry logic and error handling

--- python/app/api/endpoints/shorten.py
+++ python/app/api/endpoints/shorten.py
@@ -2,13 +2,23 @@ from fastapi import APIRouter, Request
 from nanoid import generate
 from app.schemas import LinkFromUser
 from app.db import queries
+from sqlalchemy.exc import IntegrityError
 
 router = APIRouter()
 
-@router.post("/shorten")
+@router.post("/shorten", status_code=201)
 async def shorten(link: LinkFromUser, request: Request):
-    slug = generate(size=8)
+    url = str(link.url)
     forwarded = request.headers.get("X-Forwarded-For")
     created_by = forwarded.split(",")[0] if forwarded else request.client.host
-    queries.insert_link(url=str(link.url), slug=slug, created_by=created_by)
+    
+    inserted = False
+    for _ in range(5):
+        slug = generate(size=8)
+        try:
+            print(f"inserting ${url} into db with slug ${slug}. Added by IP {created_by}")
+            queries.insert_link(url=url, slug=slug, created_by=created_by)
+        except IntegrityError:
+            continue
+    
     return { 'slug' : slug }
