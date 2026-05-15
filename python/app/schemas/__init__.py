8323e187b1dc910252d72328a5ea0fb450e50deb
gip <98342753+giplgwm@users.noreply.github.com>
2026-05-15 02:14:14 +0000

    fix schema imports

--- python/app/schemas/__init__.py
+++ python/app/schemas/__init__.py
@@ -1,2 +1,2 @@
-from python.app.schemas.link_from_user import LinkFromUser
-from python.app.schemas.link_to_user import LinkToUser
+from app.schemas.link_from_user import LinkFromUser
+from app.schemas.link_to_user import LinkToUser
