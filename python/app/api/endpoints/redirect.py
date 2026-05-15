6d2347b404907f3784ec41ef54143db350e05645
gip <98342753+giplgwm@users.noreply.github.com>
2026-05-15 03:17:21 +0000

    add /{slug} route to router

--- python/app/api/endpoints/redirect.py
+++ python/app/api/endpoints/redirect.py
@@ -1,15 +1,14 @@
 from http import HTTPStatus
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
+from app.db import queries
 
 router = APIRouter()
 
 @router.get("/{slug}")
 async def redirect(slug: str):
-    # Redirect to original url
-    pass
-    # if slug in db:
-    original_url = None
-    return RedirectResponse(original_url, HTTPStatus.TEMPORARY_REDIRECT)
-    # if slug not in db:
-    return RedirectResponse("/404", HTTPStatus.TEMPORARY_REDIRECT)
+    original_url = queries.get_link(slug=slug)
+    if original_url:
+        return RedirectResponse(original_url['url'], HTTPStatus.FOUND)
+    else:
+        return RedirectResponse("/404", HTTPStatus.FOUND)
--- python/app/api/router.py
+++ python/app/api/router.py
@@ -8,4 +8,4 @@ APIrouter = APIRouter()
 APIrouter.include_router(root.router)
 APIrouter.include_router(shorten.router)
 # # Redirect comes last to make sure /{slug} does not take priority for matching over named routes
-# APIrouter.include_router(redirect.router)
+APIrouter.include_router(redirect.router)
