1f8f901bf0bdd8de7ddaf18d1c01fb3029f29697
gip <98342753+giplgwm@users.noreply.github.com>
2026-05-15 02:40:40 +0000

    correct link to link.url in insert

--- python/app/api/endpoints/shorten.py
+++ python/app/api/endpoints/shorten.py
@@ -10,5 +10,5 @@ async def shorten(link: LinkFromUser, request: Request):
     slug = generate(size=8)
     forwarded = request.headers.get("X-Forwarded-For")
     created_by = forwarded.split(",")[0] if forwarded else request.client.host
-    queries.insert_link(url=str(link), slug=slug, created_by=created_by)
+    queries.insert_link(url=str(link.url), slug=slug, created_by=created_by)
     return { 'slug' : slug }
